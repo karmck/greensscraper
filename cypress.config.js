7bc2a694c82db0a2ea6374751c30baa354342645
Kari McKay <karimckay@gmail.com>
2022-08-23 16:27:22 +0200

    Updated index and todo

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
-  video: true,
+  video: false,
   pageLoadTimeout: 240000,
   e2e: {
     setupNodeEvents(on, config) {
